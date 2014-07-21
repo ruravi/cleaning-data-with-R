b026d33f4eb66f73e9cc08e76937b1c3db3741ec
Rukmani <lakhi.r@gmail.com>
2014-07-20 23:13:09 -0700

    Initial commit of run_analysis.R the main analysis file.

---
+++ run_analysis.R
@@ -0,0 +1,16 @@
+# This script should be run in the folder containing the Samsung data set.
+
+# Merges the training and the test sets to create one data set.
+# TODO()
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+# TODO()
+
+# Uses descriptive activity names to name the activities in the data set
+# TODO()
+
+# Appropriately labels the data set with descriptive variable names. 
+# TODO()
+
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# TODO()
