6bf30fb1c0bd55316458846c6443475052a66242
Rukmani <lakhi.r@gmail.com>
2014-07-26 14:37:27 -0700

    Code complete to output a tidy set with descriptive feature names. README file complete with all steps.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ What directory to run from
 Download the Samsung Galaxy S smartphone data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and unzip it to any directory.
 Change to inside that directory and from there run run_analysis.R from the R prompt.
 
-Requirements
+System Requirements
 ==============
 The script loads in a large data set that may not fit into the memory of all machines.
 So make sure R can have atleast 90Mb of RAM alloted to it. In other words, if your
@@ -18,17 +18,19 @@ able to run this without trouble.
 
 What the script does
 =====================
+* Loads the features contained in X_train and X_test files.
+* Extracts only the measurements on the mean and standard deviation for each measurement.
+  This is done by hard coding the column numbers of interest. 
+* Loads the file that contains subject identifiers (i.e the IDs of the subjects
+  who performed each measurement corresponding to the feature files)
+* Loads the activity labels file that associates an activity label (eg. WALKING)
+  to each measurement example.
+* Combines the subject IDs, features and activity labels for each of training and
+  test sets to get 2 big merged data sets.
 * Merges the training and the test sets to create one data set.
-
-
-* Extracts only the measurements on the mean and standard deviation for each measurement. 
-
-
-* Uses descriptive activity names to name the activities in the data set
-
-
-* Appropriately labels the data set with descriptive variable names. 
-
-* Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+* Uses descriptive activity names to name the activity labels in the data set
+* Appropriately labels the data set with descriptive variable names for each of the
+  features instead of having cryptic names such as V1, V2 etc.
+* Creates a second, independent tidy data set with the average of each variable/feature for each activity and each subject. And writes it out to a file called submission.txt
 
 
--- run_analysis.R
+++ run_analysis.R
@@ -7,32 +7,72 @@ test <- read.table("test/X_test.txt")
 
 # Extracts only the measurements on the mean and standard deviation for each measurement. The column numbers
 # are hard coded here.
-mean_feature_columns <- c(1,2,3,41,42,43,121,122,123,161,162,163,201,214,227,253,266,267,268,345,346,347,424,425,426,503,516,529,542)
+mean_feature_columns <- c(1,2,3,41,42,43,81,82,83,121,122,123,161,162,163,201,214,227,240,253,266,267,268,345,346,347,424,425,426,503,516,529,542)
 train <- train[, mean_feature_columns]
 test <- test[, mean_feature_columns]
 
-# Uses descriptive activity names to name the activities in the data set
+# Read in the ids of the subjects with whom each measurement is related to.
 subject_train <- read.table("train/subject_train.txt", col.names = c("SubjectIdentifier"))
 subject_test <- read.table("test/subject_test.txt", col.names = c("SubjectIdentifier"))
 
+# Read in the data label id  for each data point in both the training and test data.
 y_train <- read.table("train/Y_train.txt", col.names = c("ActivitylabelID"))
 y_test <- read.table("test/Y_test.txt", col.names = c("ActivitylabelID"))
 
+# Read in descriptive names associated with the activity id recorded in the Y labels file.
 activity_names <- read.table("activity_labels.txt", col.names = c("Num", "ActivityLabel"))
 
-# Clip all the data together.
+# Combine all the data together.
 all_train <- cbind(subject_train, train, y_train)
 all_test <- cbind(subject_test, test, y_test)
 all_data <- rbind(all_train, all_test)
+
+# Removing unwanted variables from the workspace to save memory. This is optional.
 remove(all_train, all_test, train, test, subject_train, subject_test, y_train, y_test)
 
-all_data <- merge(all_data, activity_names, by.x = "ActivitylabelID", by.y = "Num", all = TRUE)
-all_data$ActivitylabelID <- NULL
+# Appropriately labels the data set with descriptive variable names.
+names(all_data)[2:34] <- c("timeDomain.BodyAcceleration.X.Mean",
+                               "timeDomain.BodyAcceleration.Y.Mean",
+                               "timeDomain.BodyAcceleration.Z.Mean",
+                               "timeDomain.GravityAcceleration.X.Mean",
+                               "timeDomain.GravityAcceleration.Y.Mean",
+                               "timeDomain.GravityAcceleration.Z.Mean",
+                               "timeDomain.BodyAcceleration.LinearAcceleration.Jerk.X.Mean",
+                               "timeDomain.BodyAcceleration.LinearAcceleration.Jerk.Y.Mean",
+                               "timeDomain.BodyAcceleration.LinearAcceleration.Jerk.Z.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.X.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Y.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Z.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Jerk.X.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Jerk.Y.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Jerk.Z.Mean",
+                               "timeDomain.BodyAcceleration.Magnitude.Mean",
+                               "timeDomain.GravityAcceleration.Magnitude.Mean",
+                               "timeDomain.BodyAcceleration.LinearAcceleration.Jerk.Magnitude.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Magnitude.Mean",
+                               "timeDomain.BodyAcceleration.Gyroscope.Jerk.Magnitude.Mean",
+                               "freqDomain.BodyAcceleration.X.Mean",
+                               "freqDomain.BodyAcceleration.Y.Mean",
+                               "freqDomain.BodyAcceleration.Z.Mean",
+                               "freqDomain.BodyAcceleration.LinearAcceleration.Jerk.X.Mean",
+                               "freqDomain.BodyAcceleration.LinearAcceleration.Jerk.Y.Mean",
+                               "freqDomain.BodyAcceleration.LinearAcceleration.Jerk.Z.Mean",
+                               "freqDomain.BodyAcceleration.Gyroscope.X.Mean",
+                               "freqDomain.BodyAcceleration.Gyroscope.Y.Mean",
+                               "freqDomain.BodyAcceleration.Gyroscope.Z.Mean",
+                               "freqDomain.BodyAcceleration.Magnitude.Mean",
+                               "freqDomain.BodyAcceleration.LinearAcceleration.Jerk.Magnitude.Mean",
+                               "freqDomain.BodyAcceleration.Gyroscope.Magnitude.Mean",
+                               "freqDomain.BodyAcceleration.Gyroscope.Jerk.Magnitude.Mean")
 
-# Appropriately labels the data set with descriptive variable names. 
-# TODO()
-# Don't use the features.txt some are incorrect some have illegal characters in them making them hard to work with. Come up with your own names.
+# Use descriptive activity names to name the activities in the data set by merging the appropriate columns.
+all_data <- merge(all_data, activity_names, by.x = "ActivitylabelID", by.y = "Num", all = TRUE)
+
+# Remove the activity label id column, this is now just redundant information since we have the descriptive names.
+all_data$ActivitylabelID <- NULL
 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 melted <- melt(all_data, id.vars = c("SubjectIdentifier", "ActivityLabel"), variable.name = "V")
 result <- dcast(melted, SubjectIdentifier + ActivityLabel ~ V, mean)
+
+write.table(result,file = "submission.txt")
